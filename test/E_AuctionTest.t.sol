ad5f5cb0b5539cb400cbd89a0611946b26f581e7
DVYNE <agboolafemi03@gmail.com>
2024-12-01 23:32:03 +0100

    test initialization

--- test/E_AuctionTest.t.sol
+++ test/E_AuctionTest.t.sol
@@ -1,4 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-import {Test} from "";
+import {Test} from "lib/forge-std /src/Test.sol";
+import {ERC20Mock} from "lib/openzeppelin-contracts/contracts/mocks/ERC20Mock.sol";
+import {ERC721Mock} from "lib/openzeppelin-contracts/contracts/mocks/ERC721Mock.sol";
+
+contract E_AuctionTest is Test {
+    
+}
