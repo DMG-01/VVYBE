9c9d221048d37cfce4dd0424ee1f8c2f1af19830
DVYNE <agboolafemi03@gmail.com>
2024-12-12 16:50:17 +0100

    tokenURI getter function

--- src/NFT.sol
+++ src/NFT.sol
@@ -76,6 +76,12 @@ function generateUri(
 }
 
 
+/***************GETTER FUNCTION */
+
+function tokenURI(uint256 _tokenId) public view override returns(string memory ) {
+    return tokenIdToURI[_tokenId];
+}
+
 
 
  }
