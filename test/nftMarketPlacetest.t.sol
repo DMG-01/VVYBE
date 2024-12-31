8c093e88845338829d551d4b18d1b701b10719b3
DVYNE <agboolafemi03@gmail.com>
2024-12-31 14:44:45 +0100

    nftmarketplace test setup

--- test/nftMarketPlacetest.t.sol
+++ test/nftMarketPlacetest.t.sol
@@ -1,4 +1,4 @@
-//SPDX-license-Identifier: MIT
+//SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.13;
 
@@ -9,16 +9,28 @@ import {Script} from "lib/forge-std /src/Script.sol";
 import {NFT} from "src/NFT.sol";
 import { NFTDeployer} from "src/nftDeployer.sol";
 import {NftMarketPlace} from "src/nftMarketPlace.sol";
+import {ERC20Mock} from "lib/openzeppelin-contracts/contracts/mocks/ERC20Mock.sol";
+import {ERC721Mock} from "lib/openzeppelin-contracts/contracts/mocks/ERC721Mock.sol";
+import {console} from "lib/forge-std /src/console.sol";
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 
 contract nftMarketPlaceTest is Test {
 
     deployScript deployer;
     NftMarketPlace nftMarketPlace;
+    address USER1 =  makeAddr("USER1");
+    address  USER2 = makeAddr("USER2");
+    uint256 TOKEN_ID = 5;
 
+ 
     function setUp() public {
         deployer = new deployScript();
         (,,,nftMarketPlace) = deployer.run();
+         ERC20Mock usdt = new ERC20Mock("USDT","USDT",USER2,10E18);
+         ERC721Mock ajdNft = new ERC721Mock("AJDNFT","AJDNFT");
+         ajdNft.mint(USER1,TOKEN_ID);
+         console.log(ERC20(usdt).balanceOf(USER2));
     }
 
     function testDeployerIsAdmin() public view {
