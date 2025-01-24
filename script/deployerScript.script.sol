3ca46f7e628f84855c60d9d66a4b436b5f614929
DVYNE <agboolafemi03@gmail.com>
2025-01-24 19:41:27 +0100

    adding the bridging contract to the script

--- script/deployerScript.script.sol
+++ script/deployerScript.script.sol
@@ -7,20 +7,25 @@ import {NFT} from "src/NFT.sol";
 import { NFTDeployer} from "src/nftDeployer.sol";
 import {NftMarketPlace} from "src/nftMarketPlace.sol";
 import {BridgedNft} from "src/Bridge/bridgedNFT.sol";
+import {BurnerAddress} from "src/Bridge/burnerAddress.sol";
+//import {NftBridgeReceiverContract} from "src/Bridge/receiverContract.sol";
+//import {sourceChainNftBridge} from "src/Bridge/sourceChainNftBridge.sol";
+
 //import 
 
 
 contract deployScript is Script {
 
-    function run() external returns(E_Auction,NFT,NFTDeployer,NftMarketPlace) {
+    function run() external returns(E_Auction,NFT,NFTDeployer,NftMarketPlace,BridgedNft,BurnerAddress) {
         vm.startBroadcast();
        E_Auction e_auction = new E_Auction();
        NFT nft = new NFT("MIMI","MIMI",msg.sender);
        NFTDeployer nftDeployer = new NFTDeployer(); 
        NftMarketPlace nftMarketPlace = new NftMarketPlace();
-       
+       BridgedNft bridgedNft = new BridgedNft("vvybe","vvybe",msg.sender);
+       BurnerAddress burnerAddress = new BurnerAddress();
         vm.stopBroadcast();
-        return (e_auction,nft,nftDeployer,nftMarketPlace);
+        return (e_auction,nft,nftDeployer,nftMarketPlace,BridgedNft,burnerAddress);
 
 
     }
